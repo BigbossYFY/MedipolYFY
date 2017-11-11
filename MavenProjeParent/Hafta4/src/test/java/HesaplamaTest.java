43ed941323652991bc43d3f2771229ca94451298
bprlab <bprlab@10.200.144.20>
2017-11-11 13:56:24 +0300

    hesaplamalar

---
+++ MavenProjeParent/Hafta4/src/test/java/HesaplamaTest.java
@@ -0,0 +1,15 @@
+package h4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HesaplamaTest {
+
+	@Test
+	public void testTopla() {
+	int gercekSonuc = Hesaplama .topla(25, 14);
+	
+	}
+	
+}
